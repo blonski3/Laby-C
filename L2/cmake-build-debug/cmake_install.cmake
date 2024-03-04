2b992bbfae80e164520be56f5010cfe282122ff3
blona <bln157139@gmail.com>
2024-03-04 21:23:01 +0100

    xd

--- L2/cmake-build-debug/cmake_install.cmake
+++ L2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/bln15/Documents/GitHub/Laby C/Laby-C/L2
+# Install script for directory: C:/Users/bln15/Documents/GitHub/Laby-C/L2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/bln15/Documents/GitHub/Laby C/Laby-C/L2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/bln15/Documents/GitHub/Laby-C/L2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- L2/defs.h
+++
@@ -1,4 +0,0 @@
-float x;
-float y;
-
-float add(float x, float y);
