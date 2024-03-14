f03572ff68ec7a52b1106c3914fe0f17b3818ce1
blonski3 <bln157139@gmail.com>
2024-03-14 23:01:04 +0100

    comments

--- L3/zad2/conversion.c
+++ L3/zad2/conversion.c
@@ -18,8 +18,8 @@ void decimalToBinary(int number) {
     unsigned int originalNumber = number;
 
     while(number > 0) {
-        binary += (number % 2) * power; // reszta z dzielenia przed 2
-        power *= 10; // przesuwamy sie do kolejnego bitu w binarnym
+        binary += (number % 2) * power; // zwraca reszta z dzielenia przed 2
+        power *= 10; // przesuwamy sie do kolejnego bitu w binarnym, zwiekszajac potege
         number /= 2; // dzielimy liczbe przed 2 by przejsc do kolejnego bitu
     }
 
