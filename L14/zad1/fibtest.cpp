08642cf7cfa1dadff813355c780438207cd51ad8
blonski3 <bln157139@gmail.com>
2024-06-20 23:22:47 +0200

    L14

---
+++ L14/zad1/fibtest.cpp
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include <cassert>
+#include "fib.h"
+
+// Testy jednostkowe przy użyciu assert
+void fibtest() {
+
+    assert(fib(0) == 0);
+    assert(fib(1) == 1);
+    assert(fib(2) == 1);
+    assert(fib(3) == 2);
+    assert(fib(4) == 3);
+    assert(fib(5) == 5);
+    assert(fib(6) == 8);
+    assert(fib(10) == 55);
+    assert(fib(20) == 6765);
+    assert(fib(30) == 832040);
+
+    printf("jest ok.");
+
+}
